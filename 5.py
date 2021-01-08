5aac2dad4f2a68212dddc06e997aff5db75347ed
wanx-wan <77148609+wanx-wan@users.noreply.github.com>
2021-01-08 21:06:26 +0700

    Delete 5.py

--- 5.py
+++
@@ -1,8 +0,0 @@
-def get_box_area(width,length,height):
-    if width < 0 or length < 0 or height < 0:
-        return 0
-    box_area = width * length * height
-    return box_area
-box1 = get_box_area(4,-4,2)
-box2 = get_box_area(width=1,length=1,height=1)
-print(box1)
