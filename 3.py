cb6bb22938766e9b9316faca44b461092fed7a55
wanx-wan <77148609+wanx-wan@users.noreply.github.com>
2021-01-08 20:52:59 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+score = 80
+if score >= 80:
+    print('Grade A')
+elif score >= 70:
+    print('Grade B')
+else:
+    print('Grade C')
---
+++ 4.py
@@ -0,0 +1,4 @@
+for i in range(1,8):
+    if i %3 == 0:
+        break
+    print(i)
